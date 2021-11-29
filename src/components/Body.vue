03f3a1f6c7d5d336866b7edefc4ef52036435d26
ieeah <lbnfnc90@gmail.com>
2021-11-29 15:57:27 +0100

    done milestone 2

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -3,7 +3,8 @@
 		<div class="movie" v-for="movie in movies" :key="`${movie.title}`">
 			<h2>{{movie.title}}</h2>
 			<p>{{movie.original_title}}</p>
-			<p>{{movie.original_language}}</p>
+			<p v-if="!flags.includes(movie.original_language)">{{ movie.original_language}}</p>
+			<img v-else :src="require(`@/assets/flags/${movie.original_language}.png`)" alt="">
 			<p>{{movie.vote_average}}</p>
 		</div>
 	</div>
@@ -15,9 +16,19 @@ export default {
 	props: {
 		movies: Array,
 	},
+	data() {
+		return {
+			flags: ['it', 'en'],
+		};
+	},
 }
 </script>
 
 <style scoped lang="scss">
-
+	.movie {
+		img {
+			width: 50px;
+			height: 30px;
+		}
+	}
 </style>
