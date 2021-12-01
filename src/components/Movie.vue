71ca08af80b5dc45fbf3e05aaabb20741bbe2b0a
ieeah <lbnfnc90@gmail.com>
2021-12-01 11:06:41 +0100

    done star rating

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -7,11 +7,22 @@
 			{{originalLanguage}}
 		</p>
 		<img v-else
-			class="flag" alt=""
+			class="flag" :alt="title || name"
 			:src="require(`@/assets/flags/${originalLanguage}.png`)"
 		>
-		<p>{{voteAverage}}</p>
-		<img v-if="posterPath!= null"
+		<div class="rating">
+			<span v-for="(element, i) in Math.ceil(voteAverage / 2)"
+				:key="`rating${i}`"
+			>
+				<i class="fas fa-star"></i>
+			</span>
+			<span v-for="(element, i) in 5 - (Math.ceil(voteAverage / 2))"
+				:key="`emptyRating${i}`"
+			>
+				<i class="far fa-star"></i>
+			</span>
+		</div>
+		<img v-if="posterPath"
 			:src="`https://image.tmdb.org/t/p/w185${posterPath}`"
 			:alt="title">
 	</div>
@@ -42,5 +53,11 @@ export default {
 .flag {
 	width: 50px;
 	height: 30px;
+}
+
+.rating {
+	display: flex;
+	align-items: center;
+	color: $accent-color;
 }
 </style>
